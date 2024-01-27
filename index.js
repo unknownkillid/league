29d35b595e9b4feb45157b128ea7b306623b35b4
[R*] Dante <102482809+unknownkillid@users.noreply.github.com>
2024-01-27 20:48:21 +0400

    video background added

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ startButton.addEventListener('click', () => {
     startContainer.classList.add('startNone')
     setTimeout(() => {
         startContainer.style.display = 'none'
-        // legendsNeverDie.play();
+        legendsNeverDie.play();
+        legendsNeverDie.loop = true;
     }, 900);
 })
 
@@ -17,10 +18,38 @@ muteButton.addEventListener('click', () => {
     if (musicPlays) {
         legendsNeverDie.pause();
         musicPlays = false;
-        muteButton.innerHTML = '<i class="fa-solid fa-play"></i>' 
+        muteButton.innerHTML = '<i class="fa-solid fa-play"></i>'
     } else {
         legendsNeverDie.play();
         musicPlays = true;
-        muteButton.innerHTML = '<i class="fa-solid fa-pause"></i>' 
+        muteButton.innerHTML = '<i class="fa-solid fa-pause"></i>'
     }
 })
+
+const video = document.getElementById("scrollingVideo");
+let seeking = false;
+
+video.addEventListener("seeking", function () {
+  seeking = true;
+});
+
+video.addEventListener("seeked", function () {
+  seeking = false;
+});
+
+function animate() {
+  const lim =
+    Math.max(
+      document.body.scrollHeight,
+      document.body.offsetHeight,
+      document.documentElement.clientHeight,
+      document.documentElement.scrollHeight,
+      document.documentElement.offsetHeight
+    ) - window.innerHeight;
+  if (!seeking && video.duration) {
+    console.log(window.scrollY, lim, video.duration);
+    video.currentTime = video.duration * (window.scrollY / lim);
+  }
+  requestAnimationFrame(animate);
+}
+animate();
